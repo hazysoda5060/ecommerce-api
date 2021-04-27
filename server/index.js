d1c1df86cc2e473d513c9fec3649dd74ae1b7334
alex <alexanderstapp@gmail.com>
2021-04-27 16:09:23 -0600

    finished build

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
 
 app.get('/api/products', getProducts)
+
+app.get('/api/products/:id', getProduct)
 
 
 const port = 3064
